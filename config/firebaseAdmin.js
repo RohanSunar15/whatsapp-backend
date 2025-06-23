32919bc1d18e732ae38070c6ece16d0a604accd9
Rohan Sunar <rohanksunar15@gmail.com>
2025-06-23 21:58:01 +0530

    first commit

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./whatapp-clone-9f74c-firebase-adminsdk-fbsvc-3c5cee24f1.json'); 
+const serviceAccount = require('../whatapp-clone-9f74c-firebase-adminsdk-fbsvc-3c5cee24f1.json'); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
